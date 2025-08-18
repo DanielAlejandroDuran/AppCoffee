6096c3e93368c9c5f1312449aebc61ee46b8c223
DanielAlejandroDuran <166954580+DanielAlejandroDuran@users.noreply.github.com.>
2025-08-18 08:26:37 -0500

    feat: :sparkles: Creacion del enum Potencial Rendimiento

---
+++ src/Modules/CoffeVarieties/Domain/Enums/YieldPotential.cs
@@ -0,0 +1,11 @@
+namespace CoffeeApp.src.Modules.CoffeVarieties.Domain.Enums
+{
+    public enum YieldPotential
+    {
+        muy_bajo,
+        bajo,
+        medio,
+        alto,
+        excepcional        
+    }
+}
